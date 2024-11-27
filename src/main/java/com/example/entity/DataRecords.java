55d31b09e803097ff784cb115e19657c2d9850e0
Aidan <3369884494@qq.com>
2024-11-27 16:38:03 +0800

    示例

--- src/main/java/com/example/entity/DataRecords.java
+++ src/main/java/com/example/entity/DataRecords.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class DataRecords {
 
     private int RecordID;
@@ -21,54 +22,54 @@ public class DataRecords {
 
     private LocalDateTime UpdatedAt;
 
-    public int getRecordID() {
-        return RecordID;
-    }
-
-    public void setRecordID(int recordID) {
-        RecordID = recordID;
-    }
-
-    public String getRecordType() {
-        return RecordType;
-    }
-
-    public void setRecordType(String recordType) {
-        RecordType = recordType;
-    }
-
-    public String getData() {
-        return Data;
-    }
-
-    public void setData(String data) {
-        Data = data;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return CreatedAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        CreatedAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return UpdatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        UpdatedAt = updatedAt;
-    }
-
-    @Override
-    public String toString() {
-        return "DataRecords{" +
-                "RecordID=" + RecordID +
-                ", RecordType='" + RecordType + '\'' +
-                ", Data='" + Data + '\'' +
-                ", CreatedAt=" + CreatedAt +
-                ", UpdatedAt=" + UpdatedAt +
-                '}';
-    }
+//    public int getRecordID() {
+//        return RecordID;
+//    }
+//
+//    public void setRecordID(int recordID) {
+//        RecordID = recordID;
+//    }
+//
+//    public String getRecordType() {
+//        return RecordType;
+//    }
+//
+//    public void setRecordType(String recordType) {
+//        RecordType = recordType;
+//    }
+//
+//    public String getData() {
+//        return Data;
+//    }
+//
+//    public void setData(String data) {
+//        Data = data;
+//    }
+//
+//    public LocalDateTime getCreatedAt() {
+//        return CreatedAt;
+//    }
+//
+//    public void setCreatedAt(LocalDateTime createdAt) {
+//        CreatedAt = createdAt;
+//    }
+//
+//    public LocalDateTime getUpdatedAt() {
+//        return UpdatedAt;
+//    }
+//
+//    public void setUpdatedAt(LocalDateTime updatedAt) {
+//        UpdatedAt = updatedAt;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "DataRecords{" +
+//                "RecordID=" + RecordID +
+//                ", RecordType='" + RecordType + '\'' +
+//                ", Data='" + Data + '\'' +
+//                ", CreatedAt=" + CreatedAt +
+//                ", UpdatedAt=" + UpdatedAt +
+//                '}';
+//    }
 }
