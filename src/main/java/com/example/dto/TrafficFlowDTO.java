291ff307a70c5a766ae1477ffe53bf34ce31c7a8
silent322 <yb996274051@163.com>
2025-01-13 12:19:20 +0800

    yb1

--- src/main/java/com/example/dto/TrafficFlowDTO.java
+++ src/main/java/com/example/dto/TrafficFlowDTO.java
@@ -1,12 +1,16 @@
 package com.example.dto;
 
 import io.swagger.models.auth.In;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TrafficFlowDTO {
     LocalDate startTime;
     LocalDate endTime;
